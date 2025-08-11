e9565464e2720e9b02c6741d9ecd0da84cc67a75
CHIT SHINE MAUNG <chitshinemaung.mm@gmail.com>
2025-08-11 10:01:49 +0630

    Add project files.

---
+++ 2.Day2Assi/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Day2Assi
+{
+    internal class Program
+    {
+        static void Main(string [] args)
+        {
+            
+
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Age: ");
+            int Age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter NRC: ");
+            string nrc = Console.ReadLine();
+
+            Console.Write("Enter Gender: ");
+            string Gender = Console.ReadLine();
+
+            Console.Write("Enter Nationlity: ");
+            string nationlity = Console.ReadLine();
+
+            Console.Write("Enter Religion: ");
+            string religion = Console.ReadLine();
+
+            Console.Write("Enter Education: ");
+            string education = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Enter PhoneNumber: ");
+            string phone = Console.ReadLine();
+
+            Console.Write("Enter Address: ");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("\n \n");
+
+            Console.WriteLine("\t   CURRICULUM VITAE");
+            Console.WriteLine("\t -----------------------");
+            Console.WriteLine();
+
+            Console.WriteLine($"Name               : {name} \n");
+            Console.WriteLine($"Age                : {Age} \n");
+            Console.WriteLine($"NRC                : {nrc}  \n");
+            Console.WriteLine($"Gender             : {Gender}  \n");
+            Console.WriteLine($"Nationlity         : {nationlity}  \n");
+            Console.WriteLine($"Religion           : {religion}   \n");
+            Console.WriteLine($"Education          : {education}   \n");
+            Console.WriteLine($"Email              : {email}   \n");
+            Console.WriteLine($"PhoneNumber        : {phone}   \n");
+            Console.WriteLine($"Address            : {address}");
+
+
+
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class Program
+    {
+        static void Main(string [] args)
+        {
+
+            byte day = 10;
+            byte month = 8;
+            short year = 2025;
+
+            Console.WriteLine("To day is {0}-{1}-{2}.", day, month, year);
+
+            float f = 0.1f;
+            double d = 1.0;
+            decimal dec = 1.1m;
+            
+            char ch = 'a';
+
+        }
+    }
+}
